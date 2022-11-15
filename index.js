c76e253f6ec7511b05bb9397dd08146fc5e56fad
alejooroncoy <adoa2705@gmail.com>
2022-11-15 05:49:05 -0500

    feat: Update /hosts/list ✨👨‍💻

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import { parse } from "yaml";
 import express from "express";
-import { readdir } from "node:fs/promises";
+import { readdir, readFile } from "node:fs/promises";
 import path from "node:path";
 
 const app = express();
@@ -12,7 +13,22 @@ hostsRoute.use("/static", express.static(dbHostsServerPes6));
 
 hostsRoute.get("/list", async (_, res) => {
   const hostsName = await readdir(dbHostsServerPes6, "utf-8");
-  return res.json(hostsName);
+  const hosts = await Promise.all(
+    hostsName.map(async (hostName) => {
+      const pathConfigHostName = path.resolve(
+        dbHostsServerPes6,
+        `./${hostName}`,
+        "config.yml"
+      );
+      const contentConfig = await readFile(pathConfigHostName, "utf-8");
+      const config = parse(contentConfig);
+      return {
+        ...config,
+        description: config.description.trim(),
+      };
+    })
+  );
+  return res.json(hosts);
 });
 
 app.use("/hosts", hostsRoute);
